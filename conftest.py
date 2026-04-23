557ba995b6598973f232ca2defd4a5185c8bc497
Shubham Mahalle <shubhammahallee@gmail.com>
2026-04-23 19:33:02 +0530

    Enhance headless Chrome options in conftest.py
    Add options for headless Chrome execution.

--- conftest.py
+++ conftest.py
@@ -13,6 +13,8 @@ def setup():
   elif browser == "headless":
     options = Options()
     options.add_argument("--headless")
+    options.add_argument("--disable-gpu")  
+    options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(options=options)
   else:
         driver. webdriver.Chrome()
