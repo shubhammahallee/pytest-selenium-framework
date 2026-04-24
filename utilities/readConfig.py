00628be4b5eb9fd2726feb69feec20e72ffc6daa
Shubham Mahalle <shubhammahallee@gmail.com>
2026-04-24 09:33:35 +0530

    Update readConfig.py

--- utilities/readConfig.py
+++ utilities/readConfig.py
@@ -4,3 +4,24 @@ import os
 config = configparser.ConfigParser()
 config.read(os.path.join(os.getcwd(),"Configurations", "config.ini"))
 
+class ReadConfig:
+
+  @staticmethod
+  def getBrowser():
+    return config.get("common info", "browser")
+    
+  @staticmethod
+  def getBaseURL():
+    return config.get("common info", "browser")
+    
+  @staticmethod
+  def getUsername():
+    return config.get("common info", "browser")
+    
+  @staticmethod
+  def getPassword():  
+    return config.get("common info", "browser")
+    
+    
+    
+
