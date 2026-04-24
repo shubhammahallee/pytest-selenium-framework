3959f681611bef77acee6ff7db8abead6e74046b
Shubham Mahalle <shubhammahallee@gmail.com>
2026-04-24 10:48:12 +0530

    Add login test case with page object model

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -1,1 +1,9 @@
+from pageObjects.LoginPage import LoginPage
+from utilities.readConfig import ReadConfig
+
+def test_login(setup):
+ lp = LoginPage(setup)
+ lp.setUsername(ReadConfig.getUsername())
+ lp.setPassword(ReadConfig.getPassword())
+ lp.clickLogin()
 
