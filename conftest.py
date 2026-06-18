79be04300fb26e442faf71b06e464218c0563370
Shubham Mahalle <shubhammahallee@gmail.com>
2026-06-18 09:15:00 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,1 +1,26 @@
+import pytest
+from selenium import webdriver
+from Utilities.ReadConfig import ReadConfig
 
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+
+
+@pytest.fixture(scope="function")
+def setup(request):
+    browser = request.config.getoption("--browser")
+    if browser == "Chrome":
+        print("Running in Chrome")
+        driver = webdriver.Chrome()
+    elif browser == "Edge":
+        print("Running in Edge")
+        driver = webdriver.Edge()
+    else:
+        driver = webdriver.Chrome()
+
+    driver.maximize_window()
+
+    driver.get(ReadConfig().get_application_url())
+    request.cls.driver = driver
+    yield driver
+    driver.quit()
