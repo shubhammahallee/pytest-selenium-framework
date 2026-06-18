160309db5122cfd4527d45b5cea187feb2562e10
Shubham Mahalle <shubhammahallee@gmail.com>
2026-06-18 09:15:57 +0530

    Update Logger.py

--- utilities/Logger.py
+++ utilities/Logger.py
@@ -1,1 +1,17 @@
+import logging
+import os
+
+class Log_details:
+
+    LOG_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "Report", "test_log.log")
+
+    @staticmethod
+    def get_logger():
+        logging.basicConfig(
+            filename= "demo.log",
+            level = logging.INFO,
+            format = '%(asctime)s - %(levelname)s - %(message)s',
+            datefmt = '%Y-%m-%d %H:%M:%S %p'
+        )
+        return logging.getLogger()
 
