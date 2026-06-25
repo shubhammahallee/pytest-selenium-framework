01222de61fffb7d898199880e2fb87474d9dc374
Shubham Mahalle <shubhammahallee@gmail.com>
2026-06-25 11:13:24 +0530

    Update Logger.py

--- utilities/Logger.py
+++ utilities/Logger.py
@@ -3,7 +3,7 @@ import os
 
 class Log_details:
 
-    LOG_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "Report", "test_log.log")
+    LOG_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "Report", "test_log.log") 
 
     @staticmethod
     def get_logger():
